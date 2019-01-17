99ab2021f533f7adc955f598e3b2dba75eaad2f1
Jijeesh <jijeesh.ka@gmail.com>
2019-01-17 21:36:03 +0530

    with user passwd

--- helm-mysqlha.sh
+++ helm-mysqlha.sh
@@ -1,1 +1,2 @@
-helm install --namespace zabbix --name zabbix-db incubator/mysqlha --set persistence.storageClass=rook-ceph-block --set mysqlUser=zabbix --set mysqlPassword=zabbix123 --set mysqlDatabase=zabbixtest
+#helm install --namespace zabbix --name zabbix-db incubator/mysqlha --set persistence.storageClass=rook-ceph-block --set mysqlUser=zabbix --set mysqlPassword=zabbix123 --set mysqlDatabase=zabbixtest
+helm install --namespace zabbix --name zabbix-db incubator/mysqlha --set persistence.storageClass=rook-ceph-block --set mysqlha.mysqlUser=zabbix --set mysqlha.mysqlPassword=zabbix123 --set mysqlha.mysqlDatabase=zabbixtest
